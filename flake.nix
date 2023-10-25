a2d414391c1c11005a6ddb2e9abe2e0ca1245b83
Guillaume Fournier <>
2023-10-25 13:21:37 +0200

    Add flake based devShell

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "Dev environment for hugo & hinode";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }: 
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+         pkgs = import nixpkgs { inherit system; };
+        in {
+          devShells.default = pkgs.mkShell {
+            buildInputs = with pkgs;[
+              nodejs_20
+              go
+              dart-sass
+              hugo
+            ];
+          };
+        }
+      );
+}
--- shell.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs ? import <nixpkgs> {} }:
-  pkgs.mkShell {
-    nativeBuildInputs = with pkgs.buildPackages; [ nodejs_20 go dart-sass hugo ];
-    shellHook = ''
-    export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath [
-      pkgs.stdenv.cc.cc
-    ]}
-  '';
-}
